b5094ccfee79c96bba89540b03caf16c67de0ebc
Willian S <wsapcf@gmail.com>
2021-05-31 10:16:55 -0300

    add app loading and fonts

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
+import {useFonts, Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold} from '@expo-google-fonts/archivo'
+import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter'
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular, Archivo_500Medium,Archivo_600SemiBold,Inter_400Regular, Inter_500Medium
+  })
+  if(!fontsLoaded)
+    <AppLoading/>
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+   <Home/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
