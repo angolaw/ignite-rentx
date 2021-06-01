3bfed3a38c869e314c4e0a01bdabcbe4ea5f4fd6
Willian S <wsapcf@gmail.com>
2021-06-01 18:13:04 -0300

    make rent info in carDetails

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import { BackButton } from '../../components/BackButton'
 import { CarFeature } from '../../components/CarFeature'
-import { Container,Header,
-Details,
-RentCarData,
-Brand,
-Model,
-RentCarCost,
-Period,
-Price, 
-CarImages,
-Content,
-About,
-Footer,
-Features} from './styles'
+
 import {useTheme} from 'styled-components'
 import { ImageSlider } from '../../components/ImageSlider'
 
@@ -24,7 +12,17 @@ import Gasoline from '../../assets/gasoline.svg'
 import Exchange from '../../assets/exchange.svg'
 import { Button } from '../../components/Button'
 import { useNavigation } from '@react-navigation/native'
+import {Container, Header,CarImages,
+Content,
+Details,
+Description,
+Brand,
+Name,
+Rent,
+Period,
+Price,
 
+} from './styles'
 export function CarDetails(){
    
   const theme = useTheme()
@@ -36,60 +34,25 @@ export function CarDetails(){
      
   return (
      <Container>
-       <Header>
-          <BackButton onPress={() => {}} />
-          
-       </Header>
-       <CarImages>
-         <ImageSlider imagesUrl={['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg2.gratispng.com%2F20180704%2Fjxj%2Fkisspng-volkswagen-up-car-volkswagen-touareg-2018-volkswag-volkswagen-5b3ce033003919.9152554115307162110009.jpg&f=1&nofb=1','https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.glimport.ch%2Fdoc%2Fimages%2Fvw-up.png&f=1&nofb=1']}/>
+        <Header>
+           <BackButton color={theme.colors.shape_dark} onPress={() =>{}} />
+        </Header>
+        <CarImages>
+                    <ImageSlider imagesUrl={['https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg2.gratispng.com%2F20180704%2Fjxj%2Fkisspng-volkswagen-up-car-volkswagen-touareg-2018-volkswag-volkswagen-5b3ce033003919.9152554115307162110009.jpg&f=1&nofb=1','https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.glimport.ch%2Fdoc%2Fimages%2Fvw-up.png&f=1&nofb=1']}/>
 
-       </CarImages>
-       <Content>
-         <Details>
-            <RentCarData>
-               <Brand>Volkswagen</Brand>
-               <Model>Up! TSI</Model>
-            </RentCarData>
-            <RentCarCost>
-               <Period>AO DIA</Period>
-               <Price>{`R$ 250`}</Price>
-            </RentCarCost>
-         </Details>
-         <Features>
-            <CarFeature
-            icon={Acceleration}
-            info="3.2s" />
-            <CarFeature
-            icon={Speed}
-            info="300km/h" />
-            <CarFeature
-            icon={Exchange}
-            info="Manual" />
-            <CarFeature
-            icon={Gasoline}
-            info="2.8km/l" />
-            <CarFeature
-            icon={Force}
-            info="800hp" />
-            <CarFeature
-            icon={Force}
-            info="800hp" />
-            <CarFeature
-            icon={Force}
-            info="800hp" />
-         </Features>
-        
-         <About>
-            Este é automóvel desportivo. 
-            Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. 
-            É um belíssimo carro para quem gosta de acelerar.
-         </About>
-       </Content>
-       <Footer>
-          <Button title={"Escolher período do aluguel"} color={theme.colors.main} onPress={handleCarSchedule} />
-       </Footer>
-       
-       
-    </Container>
+        </CarImages>
+        <Content>
+           <Details>
+              <Description>
+                 <Brand>Volkswagen</Brand>
+                 <Name>UP! TSI</Name>
+              </Description>
+              <Rent>
+                 <Period>Ao dia</Period>
+                 <Price>R$ 590</Price>
+              </Rent>
+           </Details>
+        </Content>
+     </Container>
   )
 }
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,5 +1,5 @@
-import { FlatList } from "react-native";
-import { getBottomSpace, getStatusBarHeight } from "react-native-iphone-x-helper";
+import { ScrollView } from "react-native-gesture-handler";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
 
@@ -8,94 +8,60 @@ export const Container = styled.View`
   background-color: ${({theme}) => theme.colors.background_secondary}
 `
 export const Header = styled.View`
-  height:56px;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  position: absolute;
-  margin-top: ${getStatusBarHeight()};
-  margin-left:24px ;
+  position:absolute;
+  margin-top:${getStatusBarHeight()+18}px;
+  margin-left: 24px;
 `;
-
-export const CarImage = styled.Image`
-  width:100%;
-  height:${RFValue(132)}px;
-  
+export const CarImages = styled.View`
+  margin-top: ${getStatusBarHeight()+32}px;
 `;
-
-export const Details = styled.View`
-  flex-direction: row;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 38px;
-`;
-
-export const RentCarData = styled.View`
-`;
-
-export const Brand = styled.Text`
-  font-size: ${RFValue(10)}px;
-  font-family: ${({theme}) => theme.fonts.secondary_500};
-  color: ${({theme}) => theme.colors.text_detail};
-  text-transform: uppercase;
-
-`;
-
-export const Model = styled.Text`
-   font-size: ${RFValue(25)}px;
-  font-family: ${({theme}) => theme.fonts.secondary_500};
-  color: ${({theme}) => theme.colors.title};
-  margin-top: 4px;
-`;
-
-export const RentCarCost = styled.View``;
-
-export const Period = styled.Text`
-  font-size: ${RFValue(10)}px;
-  font-family: ${({theme}) => theme.fonts.secondary_500};
-  color: ${({theme}) => theme.colors.text_detail};
-  text-transform: uppercase;
-
-`;
-
-export const Price = styled.Text`
-  font-size: ${RFValue(25)}px;
-  font-family: ${({theme}) => theme.fonts.secondary_500};
-  color: ${({theme}) => theme.colors.main};
-  margin-top: 4px;
-`;
-
-export const Content = styled.ScrollView.attrs({
+export const Content = styled(ScrollView).attrs({
   contentContainerStyle:{
     padding:24,
     alignItems: 'center'
   },
-  showsVerticalScrollIndicator: false
+  showsVerticalScrollIndicator:false
+
 })`
 `;
-export const About = styled.Text`
-  font-family: ${({theme}) => theme.fonts.primary_400};
-  color: ${({theme})=>theme.colors.text};
-  font-size: ${RFValue(15)}px;
-  text-align: justify;
-  margin-top: 24px;
-  line-height:25px;
 
-`;
-export const CarImages = styled.View`
-  margin-top:${getStatusBarHeight()+32}px;
-`;
-export const Features = styled.View`
+export const Details = styled.View`
   width: 100%;
-  flex-direction:row;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  margin-top: 16px;
+  flex-direction: row;
+  align-items: center;
+  justify-content:space-between;
+  margin-top: 38px;
+`;
 
+export const Description = styled.View``;
+
+export const Brand = styled.Text`
+  font-family: ${({theme}) => theme.fonts.secondary_500};
+  color: ${({theme}) => theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+  text-transform: uppercase;
 `;
-export const Footer = styled.View`
-  width: 100%;
-  background-color: ${({theme}) => theme.colors.background_secondary};
-  padding: 24px 24px ${getBottomSpace()+24}px;
+
+export const Name = styled.Text`
+   font-family: ${({theme}) => theme.fonts.secondary_500};
+  color: ${({theme}) => theme.colors.title};
+  font-size: ${RFValue(25)}px;
+`;
+
+export const Rent = styled.View``;
+
+export const Period = styled.Text`
+  font-family: ${({theme}) => theme.fonts.secondary_500};
+  color: ${({theme}) => theme.colors.text_detail};
+  font-size: ${RFValue(10)}px;
+  text-transform: uppercase;
+`;
+
+export const Price = styled.Text`
+  font-family: ${({theme}) => theme.fonts.secondary_500};
+  color: ${({theme}) => theme.colors.main};
+  font-size: ${RFValue(25)}px;
 `;
