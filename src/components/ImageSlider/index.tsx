bc43a369cf56021d56ba9fb44f7815e7f1b0d1ad
Willian S <wsapcf@gmail.com>
2021-06-10 17:03:22 -0300

    fix file lint

--- src/components/ImageSlider/index.tsx
+++ src/components/ImageSlider/index.tsx
@@ -1,47 +1,45 @@
-import React, { useRef, useState } from 'react'
-import { FlatList, ViewToken } from 'react-native'
-import { Container, ImageIndexes, ImageIndex, CarImagesWrapper, CarImage} from './styles'
+import React, { useRef, useState } from "react";
+import { FlatList, ViewToken } from "react-native";
+import {
+  Container,
+  ImageIndexes,
+  ImageIndex,
+  CarImagesWrapper,
+  CarImage,
+} from "./styles";
 interface ImageSliderProps {
   imagesUrl: string[];
 }
 interface ChangeImageProps {
-  viewableItems:ViewToken[];
+  viewableItems: ViewToken[];
   changed: ViewToken[];
 }
-export function ImageSlider({imagesUrl}:ImageSliderProps){
-  const [imageIndex, setimageIndex] = useState(0)
-  const indexChanged = useRef((info: ChangeImageProps)=>{
-    const index = info.viewableItems[0].index!
-    setimageIndex(index)
-    
-  })
+export function ImageSlider({ imagesUrl }: ImageSliderProps) {
+  const [imageIndex, setimageIndex] = useState(0);
+  const indexChanged = useRef((info: ChangeImageProps) => {
+    const index = info.viewableItems[0].index!;
+    setimageIndex(index);
+  });
 
   return (
-     <Container>
-       <ImageIndexes>
-         {
-           imagesUrl.map((item, index) => (
-             <ImageIndex key={String(item)}  active={imageIndex === index} />
-           ))
-         }
-         
-        
-       </ImageIndexes>
-         <FlatList data={imagesUrl}
-          horizontal
-          onViewableItemsChanged={indexChanged.current}
-          showsHorizontalScrollIndicator={false}
-          keyExtractor={item => String(item)}
-          renderItem={({item}) => 
+    <Container>
+      <ImageIndexes>
+        {imagesUrl.map((item, index) => (
+          <ImageIndex key={String(item)} active={imageIndex === index} />
+        ))}
+      </ImageIndexes>
+      <FlatList
+        data={imagesUrl}
+        horizontal
+        onViewableItemsChanged={indexChanged.current}
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => String(item)}
+        renderItem={({ item }) => (
           <CarImagesWrapper>
-            <CarImage
-             source={{uri: item}}
-             resizeMode="contain"/>
+            <CarImage source={{ uri: item }} resizeMode="contain" />
           </CarImagesWrapper>
-
-            }
-         />
-         
+        )}
+      />
     </Container>
-  )
+  );
 }
