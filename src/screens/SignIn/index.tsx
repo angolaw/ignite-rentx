fcfe62c0334b2db85e5a87728bb82296aec9b9f0
Willian S <wsapcf@gmail.com>
2021-06-10 12:26:31 -0300

    add KeyboardAvoidingView on form

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, KeyboardAvoidingView } from "react-native";
 import { useTheme } from "styled-components";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
@@ -9,50 +9,52 @@ import { Footer, Container, Header, Subtitle, Title, Form } from "./styles";
 export function SignIn() {
   const theme = useTheme();
   return (
-    <Container>
-      <Header>
-        <StatusBar
-          barStyle="dark-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <Title>Estamos{"\n"}quase lá</Title>
-        <Subtitle>
-          Faça seu login para começar{"\n"}uma experiência incrível
-        </Subtitle>
-      </Header>
-      <Form>
-        <Input
-          iconName="mail"
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-        <PasswordInput
-          iconName="lock"
-          placeholder="Senha"
-          autoCorrect={false}
-          autoCapitalize="none"
-        />
-      </Form>
+    <KeyboardAvoidingView>
+      <Container>
+        <Header>
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
+          />
+          <Title>Estamos{"\n"}quase lá</Title>
+          <Subtitle>
+            Faça seu login para começar{"\n"}uma experiência incrível
+          </Subtitle>
+        </Header>
+        <Form>
+          <Input
+            iconName="mail"
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <PasswordInput
+            iconName="lock"
+            placeholder="Senha"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+        </Form>
 
-      <Footer>
-        <Button
-          title="Login"
-          onPress={() => {}}
-          enabled={false}
-          loading={false}
-        />
-        <Button
-          title="Criar conta gratuita"
-          onPress={() => {}}
-          enabled={false}
-          light
-          color={theme.colors.background_secondary}
-          loading={false}
-        />
-      </Footer>
-    </Container>
+        <Footer>
+          <Button
+            title="Login"
+            onPress={() => {}}
+            enabled={false}
+            loading={false}
+          />
+          <Button
+            title="Criar conta gratuita"
+            onPress={() => {}}
+            enabled={false}
+            light
+            color={theme.colors.background_secondary}
+            loading={false}
+          />
+        </Footer>
+      </Container>
+    </KeyboardAvoidingView>
   );
 }
