bf5585c47ddbfe35bbd03cddb32476fd6e205a84
Willian S <wsapcf@gmail.com>
2021-06-12 10:57:26 -0300

    add carSchema to watermelonDB

--- src/database/schema/index.ts
+++ src/database/schema/index.ts
@@ -1,9 +1,10 @@
 import { appSchema } from "@nozbe/watermelondb";
+import { carSchema } from "./carSchema";
 
 import { userSchema } from "./userSchema";
 
 const schemas = appSchema({
   version:1,
-  tables: [userSchema]
+  tables: [userSchema, carSchema]
 })
 export {schemas}
